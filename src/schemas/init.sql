3655b84bfe6ef7c025753499328f1332dbb71341
Andrey Bogdanov <bogdanov6391@gmail.com>
2019-01-27 22:26:07 +0300

    update schema

--- src/schemas/init.sql
+++ src/schemas/init.sql
@@ -1,13 +1,17 @@
 DROP TABLE IF EXISTS contacts;
 CREATE TABLE contacts
 (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100),
-    phone_number VARCHAR(50),
-    country_code CHAR(2),
-    timezone VARCHAR(20),
-    inserted_on DATETIME DEFAULT now(),
-    updated_on DATETIME DEFAULT now()
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100),
+  phone_number VARCHAR(50),
+  country_code CHAR(2),
+  timezone VARCHAR(20),
+  inserted_on DATETIME DEFAULT now(),
+  updated_on DATETIME DEFAULT now(),
+  deleted BOOL DEFAULT FALSE NULL
 );
-CREATE UNIQUE INDEX contacts_id_uindex ON phonebook.contacts (id);
+CREATE UNIQUE INDEX contacts_id_uindex ON contacts (id);
+
+
+
